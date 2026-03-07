d53dc166bd735e655bfda83cbdda54e35dfbc3f0
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2026-03-07 11:09:09 -0500

    refactor output structure to include node name, IP, and domain information

--- output.tf
+++ output.tf
@@ -1,6 +1,10 @@
-output "node_ips" {
-  description = "IPs de todos los nodos creados"
+output "node_info" {
+  description = "Información de todos los nodos creados"
   value = {
-    for key, node in digitalocean_droplet.nodes : key => node.ipv4_address
+    for key, node in digitalocean_droplet.nodes : key => {
+      name   = node.name
+      ip     = node.ipv4_address
+      domain = "${cloudflare_dns_record.node_dns[key].name}.${data.cloudflare_zone.main.name}"
+    }
   }
 }
